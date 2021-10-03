5653b2d30bb004dfe6e0466d06625932e29c8efd
Polyakov Sergey <polyakovsa@ozon.ru>
2021-10-03 11:10:42 +0300

    Add supplyShipped event

---
+++ src/CSharpCourse.Core.Lib/Events/SupplyShippedEvent.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using CSharpCourse.Core.Lib.Abstractions;
+
+namespace CSharpCourse.Core.Lib.Events
+{
+    public class SupplyShippedEvent: IKafkaMessage
+    {
+        public long SupplyId { get; set; }
+        public ICollection<Item> Items { get; set; }
+        
+        public class Item
+        {
+            public long SkuId { get; set; }
+            public long Quantity { get; set; }
+        }
+
+    }
+}
